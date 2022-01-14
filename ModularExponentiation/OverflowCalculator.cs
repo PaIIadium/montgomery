45ef2c9df6eef575cbde2fed4657e497a1b4b436
vladyslav.kuchin <vladyslav.kuchin@tatemgames.com>
2022-01-14 02:01:50 +0200

    Optimized calculation of overflow bit

--- ModularExponentiation/OverflowCalculator.cs
+++ ModularExponentiation/OverflowCalculator.cs
@@ -4,12 +4,7 @@
     {
         public static bool CalculateAdditionOverflow(bool bit1, bool bit2, bool prevOverflow)
         {
-            if (bit1)
-            {
-                return bit2 || prevOverflow;
-            }
-
-            return bit2 && prevOverflow;
+            return bit1 && bit2 || prevOverflow && bit2 || bit1 && prevOverflow;
         }
 
         public static bool CalculateSubtractionOverflow(bool bit1, bool bit2, bool prevOverflow)
